706293905a3cd052ff6be0ce5d46263016b6d0e3
Pietro Incardona <incardon@mpi-cbg.de>
2015-10-28 02:28:50 +0100

    Adding missing files

---
+++ src/util/grid_dist_testing.hpp
@@ -0,0 +1,18 @@
+/*
+ * grid_dist_testing.hpp
+ *
+ *  Created on: Oct 28, 2015
+ *      Author: i-bird
+ */
+
+#ifndef OPENFPM_NUMERICS_SRC_UTIL_GRID_DIST_TESTING_HPP_
+#define OPENFPM_NUMERICS_SRC_UTIL_GRID_DIST_TESTING_HPP_
+
+template<unsigned int dim>
+class grid_dist_testing
+{
+	grid_cpu<dim,scalar<size_t>> grid_test;
+};
+
+
+#endif /* OPENFPM_NUMERICS_SRC_UTIL_GRID_DIST_TESTING_HPP_ */
