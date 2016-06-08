4beea2a465e426109cdad71471dc0f0656bd0763
Pietro Incardona <i-bird@localhost.localdomain>
2016-06-08 23:40:06 +0200

    Adding build numeric script

---
+++ build_numerics.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "BUILDING NUMERICS"
+
+curl -X POST --data "payload={\"icon_emoji\": \":jenkins:\", \"username\": \"jenkins\"  , \"attachments\":[{ \"title\":\"Info:\", \"color\": \"#00FF00\", \"text\":\"$2 completed succeffuly the openfpm_numerics test \" }] }" https://hooks.slack.com/services/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
+
