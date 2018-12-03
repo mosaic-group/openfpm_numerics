ed9acb932781ab021ba8eb3705b7b4704d0569be
Pietro Incardona <incardon@mpi-cbg.de>
2018-12-03 18:50:14 +0100

    Fixing Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "config.h"
 #undef VERSION
 #define BOOST_TEST_MODULE "C++ test module for OpenFPM_numerics project"
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 
 #include "unit_test_init_cleanup.hpp"
 
@@ -10,3 +10,16 @@
 #include "util/util_num_unit_tests.hpp"
 #include "PSE/Kernels_unit_tests.hpp"
 #include "Draw/DrawParticles_unit_tests.hpp"
+
+
+// initialization function:
+bool init_unit_test()
+{
+  return true;
+}
+
+// entry point:
+int main(int argc, char* argv[])
+{
+  return boost::unit_test::unit_test_main( &init_unit_test, argc, argv );
+}
