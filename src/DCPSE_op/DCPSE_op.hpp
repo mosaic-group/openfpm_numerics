d4ee22dadf02fcdeb0be983523a643d93e052311
Pietro Incardona <incardon@mpi-cbg.de>
2020-01-07 14:01:10 +0100

    Starting DCPSE

---
+++ src/DCPSE_op/DCPSE_op.hpp
@@ -0,0 +1,13 @@
+/*
+ * DCPSE_op.hpp
+ *
+ *  Created on: Jan 7, 2020
+ *      Author: i-bird
+ */
+
+#ifndef DCPSE_OP_HPP_
+#define DCPSE_OP_HPP_
+
+
+
+#endif /* DCPSE_OP_HPP_ */
---
+++ src/DCPSE_op/DCPSE_op_test.cpp
@@ -0,0 +1,22 @@
+/*
+ * DCPSE_op_test.cpp
+ *
+ *  Created on: Jan 7, 2020
+ *      Author: i-bird
+ */
+
+
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+
+BOOST_AUTO_TEST_SUITE( dcpse_op_suite_tests )
+
+BOOST_AUTO_TEST_CASE( dcpse_op_tests )
+{
+	std::cout << "TEST" << std::endl;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
