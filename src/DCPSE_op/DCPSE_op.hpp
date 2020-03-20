398f0edd1ff3e49ba45a92c5ae75f00e75da4927
Pietro Incardona <incardon@mpi-cbg.de>
2020-03-20 20:29:42 +0100

    Adding tensor rank 2 slicing

--- src/DCPSE_op/DCPSE_op.hpp
+++ src/DCPSE_op/DCPSE_op.hpp
@@ -964,35 +964,6 @@ public:
     }
 };
 
-template<typename operand_type1, typename operand_type2>
-class plus
-{
-	operand_type1 op1;
-	operand_type2 op2;
-
-public:
-	typedef int it_is_a_node;
-
-	plus(const operand_type1 & op1, const operand_type2 & op2)
-	:op1(op1),op2(op2)
-	{}
-
-	void value()
-	{
-		//op1.value() + op.value;
-	}
-
-};
-
-class Field
-{
-	typedef int it_is_a_node;
-
-	void value()
-	{
-		// add non zero
-	}
-};
 
 //template<typename operand_type1, typename operand_type2/*, typename sfinae=typename std::enable_if<
 //																						std::is_same<typename operand_type1::it_is_a_node,int>::value
