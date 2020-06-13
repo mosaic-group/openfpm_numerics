4106d6f730579496e03ad5ff965e1151762bcccf
Pietro Incardona <incardon@mpi-cbg.de>
2020-06-13 12:32:36 +0200

    Fixing run numerics

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ cd ..
 source $HOME/openfpm_vars_$branch
 cd openfpm_numerics
 
-mpirun -np $3 ../build/openfpm_numerics/src/numerics
+mpirun -np $3 build/openfpm_numerics/src/numerics
 if [ $? -ne 0 ]; then
    curl -X POST --data "payload={\"icon_emoji\": \":jenkins:\", \"username\": \"jenkins\"  , \"attachments\":[{ \"title\":\"Error:\", \"color\": \"#FF0000\", \"text\":\"$2 failed to complete the openfpm_numerics test \" }] }" https://hooks.slack.com/services/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
    exit 1 ;
