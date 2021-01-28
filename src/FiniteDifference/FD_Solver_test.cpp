bd34646fe4e88c8720ed87fd1018f379485f6187
absingh <absingh@mpi-cbg.de>
2021-01-28 14:54:08 +0530

    non linear lid working

--- src/FiniteDifference/FD_Solver_test.cpp
+++ src/FiniteDifference/FD_Solver_test.cpp
@@ -36,8 +36,87 @@ struct equations2d1 {
 
     typedef petsc_solver<double> solver_type;
 };
+//! Specify the general characteristic of system to solve
+struct equations2d1e {
+
+    //! dimensionaly of the equation ( 3D problem ...)
+    static const unsigned int dims=2;
+    //! number of fields in the system
+    static const unsigned int nvar=1;
+
+    //! boundary at X and Y
+    static const bool boundary[];
+
+    //! type of space float, double, ...
+    typedef double stype;
+
+    //! type of base particles
+    typedef grid_dist_id<dims, double, aggregate<double,double,double>> b_part;
+
+    //! type of SparseMatrix for the linear solver
+    typedef SparseMatrix<double, int, EIGEN_BASE> SparseMatrix_type;
+
+    //! type of Vector for the linear solver
+    typedef Vector<double, EIGEN_BASE> Vector_type;
+
+    typedef umfpack_solver<double> solver_type;
+};
+//! Specify the general characteristic of system to solve
+struct equations2d2 {
+
+    //! dimensionaly of the equation ( 3D problem ...)
+    static const unsigned int dims=2;
+    //! number of fields in the system
+    static const unsigned int nvar=2;
+
+    //! boundary at X and Y
+    static const bool boundary[];
+
+    //! type of space float, double, ...
+    typedef double stype;
+
+    //! type of base particles
+    typedef grid_dist_id<dims, double, aggregate<double,double,double>> b_part;
+
+    //! type of SparseMatrix for the linear solver
+    typedef SparseMatrix<double, int, PETSC_BASE> SparseMatrix_type;
+
+    //! type of Vector for the linear solver
+    typedef Vector<double, PETSC_BASE> Vector_type;
+
+    typedef petsc_solver<double> solver_type;
+};
+
+struct equations2d2e {
+
+    //! dimensionaly of the equation ( 3D problem ...)
+    static const unsigned int dims=2;
+    //! number of fields in the system
+    static const unsigned int nvar=2;
+
+    //! boundary at X and Y
+    static const bool boundary[];
+
+    //! type of space float, double, ...
+    typedef double stype;
+
+    //! type of base particles
+    typedef grid_dist_id<dims, double, aggregate<double,double,double>> b_part;
+
+    //! type of SparseMatrix for the linear solver
+    typedef SparseMatrix<double, int, EIGEN_BASE> SparseMatrix_type;
+
+    //! type of Vector for the linear solver
+    typedef Vector<double, EIGEN_BASE> Vector_type;
+
+    typedef umfpack_solver<double> solver_type;
+};
 
 const bool equations2d1::boundary[] = {NON_PERIODIC,NON_PERIODIC};
+const bool equations2d1e::boundary[] = {NON_PERIODIC,NON_PERIODIC};
+const bool equations2d2::boundary[] = {NON_PERIODIC,NON_PERIODIC};
+const bool equations2d2e::boundary[] = {NON_PERIODIC,NON_PERIODIC};
+
 
 //! Specify the general characteristic of system to solve
 struct equations2d1_stag {
@@ -150,7 +229,7 @@ BOOST_AUTO_TEST_CASE(solver_check_diagonal)
         FD::Lap Lap;
         FD::LInfError LInfError;
 
-/*        Solver.impose(Lap(v),{1,1},{79,79}, prop_id<1>());
+/*      Solver.impose(Lap(v),{1,1},{79,79}, prop_id<1>());
         Solver.impose(v,{0,0},{80,0}, prop_id<0>());
         Solver.impose(v,{0,1},{0,79}, prop_id<0>());
         Solver.impose(v,{0,80},{80,80}, prop_id<0>());
@@ -240,6 +319,194 @@ BOOST_AUTO_TEST_CASE(solver_check_diagonal)
         //std::cout << "Maximum Error: " << worst << std::endl;
         //domain.write("FDSOLVER_Lap_test");
     }
+    BOOST_AUTO_TEST_CASE(Lid_driven_PC)
+    {using namespace FD;
+        timer tt2;
+        tt2.start();
+        size_t gd_sz = 82;
+        double Re=100;
+        double V_err_eps = 1e-2;
+        double alpha=0.0125;
+        constexpr int x = 0;
+        constexpr int y = 1;
+        const size_t sz[2] = {gd_sz,gd_sz};
+        Box<2, double> box({0, 0}, {1,1});
+        periodicity<2> bc = {NON_PERIODIC, NON_PERIODIC};
+        double spacing;
+        spacing = 1.0 / (sz[0] - 1);
+        Ghost<2,long int> ghost(1);
+        auto &v_cl = create_vcluster();
+        typedef aggregate<double, VectorS<2, double>, VectorS<2, double>,VectorS<2, double>,double,VectorS<2, double>,double,double> LidCavity;
+        grid_dist_id<2, double, LidCavity> domain(sz, box,ghost,bc);
+        double x0, y0, x1, y1;
+        x0 = box.getLow(0);
+        y0 = box.getLow(1);
+        x1 = box.getHigh(0);
+        y1 = box.getHigh(1);
+        auto it = domain.getDomainIterator();
+        while (it.isNext())
+        {
+            auto key = it.get();
+            auto gkey = it.getGKey(key);
+            double x = gkey.get(0) * domain.spacing(0);
+            double y = gkey.get(1) * domain.spacing(1);
+            if (y==1)
+            {domain.get<1>(key) = 1.0;}
+            else
+            {domain.get<1>(key) = 0.0;}
+
+            ++it;
+        }
+
+        domain.ghost_get<0>();
+        Derivative_x Dx;
+        Derivative_y Dy;
+        Derivative_xx Dxx;
+        Derivative_yy Dyy;
+        auto P = getV<0>(domain);
+        auto V = getV<1>(domain);
+        auto RHS = getV<2>(domain);
+        auto dV = getV<3>(domain);
+        auto div = getV<4>(domain);
+        auto V_star = getV<5>(domain);
+        auto H = getV<6>(domain);
+        auto dP = getV<7>(domain);
+
+        //0 doesnt work
+        P = 0.0*V[x];
+        int n = 0, nmax = 50, ctr = 0, errctr=1, Vreset = 0;
+        double V_err=1;
+        if (Vreset == 1) {
+            Vreset = 0;
+        }
+        P=0*V[x];
+        eq_id vx,vy;
+        vx.setId(0);
+        vy.setId(1);
+
+        double sum, sum1, sum_k,V_err_old;
+        auto StokesX=(V[x]*Dx(V_star[x])+V[y]*Dy(V_star[x]))-(1.0/Re)*(Dxx(V_star[x])+Dyy(V_star[x]));
+        auto StokesY=(V[x]*Dx(V_star[y])+V[y]*Dy(V_star[y]))-(1.0/Re)*(Dxx(V_star[y])+Dyy(V_star[y]));
+        petsc_solver<double> solverPetsc;
+        solverPetsc.setSolver(KSPGMRES);
+
+        RHS[x] = 0.0;
+        RHS[y] = 0.0;
+        dV[x]=0;
+        dV[y]=0;
+
+        timer tt;
+        while (V_err >= V_err_eps && n <= nmax) {
+            //if (n%5==0){
+                domain.ghost_get<0,1,2,3,4,5,6,7>(SKIP_LABELLING);
+                domain.write_frame("LID",n,BINARY);
+                domain.ghost_get<0>();
+            //}
+            tt.start();
+            domain.ghost_get<0>(SKIP_LABELLING);
+            RHS[x] = Dx(P);
+            RHS[y] = Dy(P);
+            div=-1.0*RHS[x];
+            FD_scheme<equations2d2,decltype(domain)> Solver(ghost,domain);
+            Solver.impose(StokesX, {1,1},{80,80}, prop_id<4>(), vx);
+            div=-1.0*RHS[y];
+            Solver.impose(StokesY, {1,1},{80,80}, prop_id<4>(), vy);
+            Solver.impose(V_star[x], {0,81},{81,81}, 1.0, vx);
+            Solver.impose(V_star[y], {0,81},{81,81}, 0.0, vy);
+            Solver.impose(V_star[x], {0,1},{0,80}, 0.0, vx);
+            Solver.impose(V_star[y], {0,1},{0,80}, 0.0, vy);
+            Solver.impose(V_star[x], {81,1},{81,80}, 0.0, vx);
+            Solver.impose(V_star[y], {81,1},{81,80}, 0.0, vy);
+            Solver.impose(V_star[x], {0,0},{81,0}, 0.0, vx);
+            Solver.impose(V_star[y], {0,0},{81,0}, 0.0, vy);
+            auto A=Solver.getA();
+            A.write("Matrix");
+            Solver.solve(V_star[x], V_star[y]);
+            //return;
+            //Solver.solve_with_solver(solverPetsc,V_star[x], V_star[y]);
+
+            domain.ghost_get<5>();
+            div = (Dx(V_star[x]) + Dy(V_star[y]));
+
+            FD_scheme<equations2d1e,decltype(domain)> SolverH(ghost,domain);
+            auto Helmholtz = Dxx(H)+Dyy(H);
+            SolverH.impose(Helmholtz,{1,1},{80,80},prop_id<4>());
+            SolverH.impose(Dy(H), {0,81},{81,81},0);
+            SolverH.impose(Dx(H), {81,1},{81,80},0);
+            SolverH.impose(Dx(H), {0,0},{81,0},0,vx,true);
+            SolverH.impose(H, {0,0},{0,0},0);
+            SolverH.impose(Dy(H), {0,1},{0,80},0);
+            //SolverH.solve_with_solver(solverPetsc2,H);
+            SolverH.solve(H);
+            domain.ghost_get<6>();
+            //dP_bulk=Bulk_Lap(H);
+            P = P - alpha*(div-0.5*(V[x]*Dx(H)+V[y]*Dy(H)));
+            //dV[x]=Dx(H);
+            //dV[y]=Dy(H);
+            V_star[0] = V_star[0] - Dx(H);
+            V_star[1] = V_star[1] - Dy(H);
+            sum = 0;
+            sum1 = 0;
+            auto it2 = domain.getDomainIterator();
+            while (it2.isNext()) {
+                auto p = it2.get();
+                sum += (domain.getProp<5>(p)[0] - domain.getProp<1>(p)[0]) *
+                       (domain.getProp<5>(p)[0] - domain.getProp<1>(p)[0]) +
+                       (domain.getProp<5>(p)[1] - domain.getProp<1>(p)[1]) *
+                       (domain.getProp<5>(p)[1] - domain.getProp<1>(p)[1]);
+                sum1 += domain.getProp<5>(p)[0] * domain.getProp<5>(p)[0] +
+                        domain.getProp<5>(p)[1] * domain.getProp<5>(p)[1];
+                ++it2;
+            }
+
+            V[x] = V_star[x];
+            V[y] = V_star[y];
+            v_cl.sum(sum);
+            v_cl.sum(sum1);
+            v_cl.execute();
+            sum = sqrt(sum);
+            sum1 = sqrt(sum1);
+            V_err_old = V_err;
+            V_err = sum / sum1;
+            if (V_err > V_err_old || abs(V_err_old - V_err) < 1e-8) {
+                errctr++;
+                //alpha_P -= 0.1;
+            } else {
+                errctr = 0;
+            }
+            if (n > 3) {
+                if (errctr > 5) {
+                    //std::cout << "CONVERGENCE LOOP BROKEN DUE TO INCREASE/VERY SLOW DECREASE IN ERROR" << std::endl;
+                    std::cout << "Alpha Halfed DUE TO INCREASE/VERY SLOW DECREASE IN ERROR" << std::endl;
+                    //alpha=alpha/2;
+                    Vreset = 1;
+                    errctr=0;
+                    //break;
+                } else {
+                    Vreset = 0;
+                }
+            }
+            n++;
+            tt.stop();
+            if (v_cl.rank() == 0) {
+                std::cout << "Rel l2 cgs err in V = " << V_err << " at " << n << " and took " <<tt.getwct() <<"("<<tt.getcputime()<<") seconds(CPU)." << std::endl;
+            }
+        }
+        double worst1 = 0.0;
+        double worst2 = 0.0;
+
+        domain.write("LID_final");
+        tt2.stop();
+        if (v_cl.rank() == 0) {
+            std::cout << "The simulation took " << tt2.getcputime() << "(CPU) ------ " << tt2.getwct()
+                      << "(Wall) Seconds.";
+        }
+    }
+
+
+
+
+
 
     /*
 In 3D we use exact solution:
