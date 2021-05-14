959d033ff7945a8ba9d0e7171ee4aa14bd10ba94
absingh <absingh@mpi-cbg.de>
2021-05-14 17:35:33 +0200

    Adding another Equation Structure

--- src/DCPSE/DCPSE_op/EqnsStruct.hpp
+++ src/DCPSE/DCPSE_op/EqnsStruct.hpp
@@ -301,6 +301,31 @@ struct equations3d3EPxz {
 
     typedef umfpack_solver<double> solver_type;
 };
+
+struct equations3d3EPz {
+    //! dimensionaly of the equation ( 3D problem ...)
+    static const unsigned int dims = 3;
+    //! number of fields in the system
+    static const unsigned int nvar = 3;
+
+    //! boundary at X and Y
+    static constexpr bool boundary[]={PERIODIC, PERIODIC,PERIODIC};
+
+    //! type of space float, double, ...
+    typedef double stype;
+
+    //! type of base particles
+    typedef vector_dist<dims, double, aggregate<double>> b_part;
+
+    //! type of SparseMatrix for the linear solver
+    typedef SparseMatrix<double, int, EIGEN_BASE> SparseMatrix_type;
+
+    //! type of Vector for the linear solver
+    typedef Vector<double> Vector_type;
+
+    typedef umfpack_solver<double> solver_type;
+};
+
 struct equations3d3Pxz {
     //! dimensionaly of the equation ( 3D problem ...)
     static const unsigned int dims = 3;
