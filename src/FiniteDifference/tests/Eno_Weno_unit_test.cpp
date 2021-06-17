7b1d6797a3d7b5e8e7ab483b5c4f439c870258ad
jstark <jstark@mpi-cbg.de>
2021-06-17 16:10:12 +0200

    Added 3D and higher precision to upwind/eno/weno unit-tests.

--- src/FiniteDifference/tests/Eno_Weno_unit_test.cpp
+++ src/FiniteDifference/tests/Eno_Weno_unit_test.cpp
@@ -22,13 +22,12 @@ BOOST_AUTO_TEST_SUITE(EnoWenoTestSuite)
 	const size_t ENO_minus    = 3;
 	const size_t Error_plus   = 4;
 	const size_t Error_minus  = 5;
-	const double EPSILON = std::numeric_limits<double>::epsilon();
 	
 	double l2_norms_ENO []    = {0.084908, 0.014937, 0.002310};
 	double linf_norms_ENO []  = {0.228915, 0.047215, 0.008046};
 	
 	double l2_norms_WENO []   = {0.032770, 0.000935, 0.000052};
-	double linf_norms_WENO [] = {0.088540, 0.002453, 0.000143};
+	double linf_norms_WENO [] = {0.168860, 0.003369, 0.000232};
 	
 	BOOST_AUTO_TEST_CASE(Eno_1D_1stDerivative_test)
 	{
@@ -85,8 +84,8 @@ BOOST_AUTO_TEST_SUITE(EnoWenoTestSuite)
 			{
 				L_norms lNorms;
 				lNorms = get_l_norms_grid<Error_plus>(g_dist);
-				BOOST_CHECK_MESSAGE(lNorms.l2   < l2_norms_ENO[count] + 0.000001 + EPSILON, "Checking L2-norm ENO");
-				BOOST_CHECK_MESSAGE(lNorms.linf < linf_norms_ENO[count] + 0.000001 + EPSILON, "Checking Linf-norm "
+				BOOST_CHECK_MESSAGE(lNorms.l2   < l2_norms_ENO[count] + 0.000001, "Checking L2-norm ENO");
+				BOOST_CHECK_MESSAGE(lNorms.linf < linf_norms_ENO[count] + 0.000001, "Checking Linf-norm "
 				                                                                             "ENO");
 //				write_lnorms_to_file(N, lNorms, "l_norms_ENO_plus", "./");
 			}
@@ -94,8 +93,8 @@ BOOST_AUTO_TEST_SUITE(EnoWenoTestSuite)
 			{
 				L_norms lNorms;
 				lNorms = get_l_norms_grid<Error_minus>(g_dist);
-				BOOST_CHECK_MESSAGE(lNorms.l2   < l2_norms_ENO[count] + 0.000001 + EPSILON, "Checking L2-norm ENO");
-				BOOST_CHECK_MESSAGE(lNorms.linf < linf_norms_ENO[count] + 0.000001 + EPSILON, "Checking Linf-norm "
+				BOOST_CHECK_MESSAGE(lNorms.l2   < l2_norms_ENO[count] + 0.000001, "Checking L2-norm ENO");
+				BOOST_CHECK_MESSAGE(lNorms.linf < linf_norms_ENO[count] + 0.000001, "Checking Linf-norm "
 				                                                                             "ENO");
 //				write_lnorms_to_file(N, lNorms, "l_norms_ENO_minus", "./");
 			}
@@ -159,8 +158,8 @@ BOOST_AUTO_TEST_SUITE(EnoWenoTestSuite)
 			{
 				L_norms lNorms;
 				lNorms = get_l_norms_grid<Error_plus>(g_dist);
-				BOOST_CHECK_MESSAGE(lNorms.l2   < l2_norms_WENO[count] + 0.000001 + EPSILON, "Checking L2-norm WENO");
-				BOOST_CHECK_MESSAGE(lNorms.linf < linf_norms_WENO[count] + 0.000001 + EPSILON, "Checking Linf-norm "
+				BOOST_CHECK_MESSAGE(lNorms.l2   < l2_norms_WENO[count] + 0.000001, "Checking L2-norm WENO");
+				BOOST_CHECK_MESSAGE(lNorms.linf < linf_norms_WENO[count] + 0.000001, "Checking Linf-norm "
 																							  "WENO");
 //				write_lnorms_to_file(N, lNorms, "l_norms_WENO_plus", "./");
 			}
@@ -168,8 +167,8 @@ BOOST_AUTO_TEST_SUITE(EnoWenoTestSuite)
 			{
 				L_norms lNorms;
 				lNorms = get_l_norms_grid<Error_minus>(g_dist);
-				BOOST_CHECK_MESSAGE(lNorms.l2   < l2_norms_WENO[count] + 0.000001 + EPSILON, "Checking L2-norm WENO");
-				BOOST_CHECK_MESSAGE(lNorms.linf < linf_norms_WENO[count] + 0.000001 + EPSILON, "Checking Linf-norm "
+				BOOST_CHECK_MESSAGE(lNorms.l2   < l2_norms_WENO[count] + 0.000001, "Checking L2-norm WENO");
+				BOOST_CHECK_MESSAGE(lNorms.linf < linf_norms_WENO[count] + 0.000001, "Checking Linf-norm "
 				                                                                              "WENO");
 //				write_lnorms_to_file(N, lNorms, "l_norms_WENO_minus", "./");
 			}
@@ -178,16 +177,16 @@ BOOST_AUTO_TEST_SUITE(EnoWenoTestSuite)
 		}
 	}
 	
-	BOOST_AUTO_TEST_CASE(Eno_2D_1stDerivative_test)
+	BOOST_AUTO_TEST_CASE(Eno_3D_test)
 	{
 		int count = 0;
 		for(size_t N=32; N<=128; N*=2)
 		{
-			const size_t grid_dim = 2;
-			const size_t sz[grid_dim] = {N, N};
+			const size_t grid_dim = 3;
+			const size_t sz[grid_dim] = {N, N, N};
 			const double box_lower = -1.0;
 			const double box_upper = 1.0;
-			Box<grid_dim, double> box({box_lower, box_lower}, {box_upper, box_upper});
+			Box<grid_dim, double> box({box_lower, box_lower, box_lower}, {box_upper, box_upper, box_upper});
 			Ghost<grid_dim, long int> ghost(3);
 			typedef aggregate<double, Point<grid_dim, double>, Point<grid_dim, double>, Point<grid_dim, double>, double, double> props;
 			typedef grid_dist_id<grid_dim, double, props> grid_in_type;
@@ -230,21 +229,91 @@ BOOST_AUTO_TEST_SUITE(EnoWenoTestSuite)
 			{
 				L_norms lNorms;
 				lNorms = get_l_norms_grid<Error_plus>(g_dist);
-				BOOST_CHECK_MESSAGE(lNorms.l2   < l2_norms_ENO[count] + 0.000001 + EPSILON, "Checking L2-norm ENO");
-				BOOST_CHECK_MESSAGE(lNorms.linf < linf_norms_ENO[count] + 0.000001 + EPSILON, "Checking Linf-norm "
+				BOOST_CHECK_MESSAGE(lNorms.l2   < l2_norms_ENO[count] + 0.000001, "Checking L2-norm ENO");
+				BOOST_CHECK_MESSAGE(lNorms.linf < linf_norms_ENO[count] + 0.000001, "Checking Linf-norm "
 				                                                                              "ENO");
-//				write_lnorms_to_file(N, lNorms, "l_norms_2D_ENO_plus", "./");
+//				write_lnorms_to_file(N, lNorms, "l_norms_3D_ENO_plus", "./");
 			}
 			
 			{
 				L_norms lNorms;
 				lNorms = get_l_norms_grid<Error_minus>(g_dist);
-				BOOST_CHECK_MESSAGE(lNorms.l2   < l2_norms_ENO[count] + 0.000001 + EPSILON, "Checking L2-norm ENO");
-				BOOST_CHECK_MESSAGE(lNorms.linf < linf_norms_ENO[count] + 0.000001 + EPSILON, "Checking Linf-norm "
+				BOOST_CHECK_MESSAGE(lNorms.l2   < l2_norms_ENO[count] + 0.000001, "Checking L2-norm ENO");
+				BOOST_CHECK_MESSAGE(lNorms.linf < linf_norms_ENO[count] + 0.000001, "Checking Linf-norm "
 				                                                                             "ENO");
-//				write_lnorms_to_file(N, lNorms, "l_norms_2D_ENO_minus", "./");
+//				write_lnorms_to_file(N, lNorms, "l_norms_3D_ENO_minus", "./");
 			}
-//			g_dist.write("grid_gaussian_2D_N" + std::to_string(N), FORMAT_BINARY);
+//			g_dist.write("grid_gaussian_3D_N" + std::to_string(N), FORMAT_BINARY);
+			count++;
+		}
+	}
+	BOOST_AUTO_TEST_CASE(Weno_3D_test)
+	{
+		int count = 0;
+		for(size_t N=32; N<=128; N*=2)
+		{
+			const size_t grid_dim = 3;
+			const size_t sz[grid_dim] = {N, N, N};
+			const double box_lower = -1.0;
+			const double box_upper = 1.0;
+			Box<grid_dim, double> box({box_lower, box_lower, box_lower}, {box_upper, box_upper, box_upper});
+			Ghost<grid_dim, long int> ghost(3);
+			typedef aggregate<double, Point<grid_dim, double>, Point<grid_dim, double>, Point<grid_dim, double>, double, double> props;
+			typedef grid_dist_id<grid_dim, double, props> grid_in_type;
+			grid_in_type g_dist(sz, box, ghost);
+			
+			g_dist.setPropNames({"f_gaussian", "df_gaussian", "WENO_plus", "WENO_minus", "Error_plus", "Error_minus"});
+			
+			double mu = 0.5 * (box_upper - abs(box_lower));
+			double sigma = 0.1 * (box_upper - box_lower);
+			
+			auto gdom = g_dist.getDomainGhostIterator();
+			while (gdom.isNext())
+			{
+				auto key = gdom.get();
+				Point<grid_dim, double> p = g_dist.getPos(key);
+				// Initialize grid and ghost with gaussian function
+				g_dist.getProp<f_gaussian>(key)  = gaussian(p, mu, sigma);
+				++gdom;
+			}
+			
+			auto dom = g_dist.getDomainIterator();
+			while (dom.isNext())
+			{
+				auto key = dom.get();
+				Point<grid_dim, double> p = g_dist.getPos(key);
+				for (int d = 0; d < grid_dim; d++)
+				{
+					g_dist.getProp<df_gaussian>(key)[d] = hermite_polynomial(p.get(d), sigma, 1) * g_dist.getProp<f_gaussian>(key);
+					g_dist.getProp<WENO_plus>(key)[d]    = WENO_5_Plus<f_gaussian>(g_dist, key, d);
+					g_dist.getProp<WENO_minus>(key)[d]   = WENO_5_Minus<f_gaussian>(g_dist, key, d);
+				}
+				
+				++dom;
+			}
+			
+			// Get the error between analytical and numerical solution
+			get_relative_error<df_gaussian, WENO_plus, Error_plus>(g_dist);
+			get_relative_error<df_gaussian, WENO_minus, Error_minus>(g_dist);
+			
+			{
+				L_norms lNorms;
+				lNorms = get_l_norms_grid<Error_plus>(g_dist);
+				BOOST_CHECK_MESSAGE(lNorms.l2   < l2_norms_WENO[count] + 0.000001, "Checking L2-norm WENO");
+				BOOST_CHECK_MESSAGE(lNorms.linf < linf_norms_WENO[count] + 0.000001, "Checking Linf-norm "
+				                                                                    "WENO");
+//				write_lnorms_to_file(N, lNorms, "l_norms_3D_WENO_plus", "./");
+			}
+			
+			{
+				L_norms lNorms;
+				lNorms = get_l_norms_grid<Error_minus>(g_dist);
+				BOOST_CHECK_MESSAGE(lNorms.l2   < l2_norms_WENO[count] + 0.000001, "Checking L2-norm WENO");
+				BOOST_CHECK_MESSAGE(lNorms.linf < linf_norms_WENO[count] + 0.000001, "Checking Linf-norm "
+				                                                                    "WENO");
+//				write_lnorms_to_file(N, lNorms, "l_norms_3D_WENO_minus", "./");
+			}
+//			g_dist.write("grid_gaussian_3D_N" + std::to_string(N), FORMAT_BINARY);
 			count++;
 		}
 	}
--- src/level_set/redistancing_Sussman/tests/l_norms/LNorms.hpp
+++ src/level_set/redistancing_Sussman/tests/l_norms/LNorms.hpp
@@ -76,7 +76,7 @@ void get_relative_error(gridtype & grid)
 	{
 		auto key = dom.get();
 		grid.template getProp<Error> (key) = abs( 1 - (grid.template getProp<PropNumeric> (key) / (grid.template
-				getProp<PropAnalytic> (key) + EPSILON)) );
+				getProp<PropAnalytic> (key))) );
 		++dom;
 	}
 }
@@ -139,6 +139,21 @@ L_norms get_l_norms_vector(vectortype & vd)
 	double linf = maxError;
 	return {l2, linf};
 }
+/**@brief Converts value into string maintaining a desired precision.
+ *
+ * @tparam T Template type of vlaue.
+ * @param myValue Value of type T.
+ * @param n Number of digits after the point the string of the value should have
+ * @return String containing myValue with precision n.
+ */
+template <typename T>
+std::string to_string_with_precision(const T myValue, const size_t n = 6)
+{
+	std::ostringstream out;
+	out.precision(n);
+	out << std::fixed << myValue;
+	return out.str();
+}
 /**@brief Writes the N (number of grid points on a square grid) and L-norms as strings to a csv-file.
  *
  * @param N Size_t variable that contains the grid size in number of grid points in one dimension for an NxN(xN) grid
@@ -157,8 +172,8 @@ static void write_lnorms_to_file(size_t N, L_norms l_norms, std::string filename
 		
 		std::ofstream l_out;
 		l_out.open(path_output_lnorm, std::ios_base::app); // append instead of overwrite
-		l_out << std::to_string(N) << ',' << std::to_string(l_norms.l2) << ',' << std::to_string(l_norms.linf)
-				<< std::endl;
+		l_out << std::to_string(N) << ',' << to_string_with_precision(l_norms.l2, 15)
+		<< ',' << to_string_with_precision(l_norms.linf) << std::endl;
 		l_out.close();
 	}
 }
