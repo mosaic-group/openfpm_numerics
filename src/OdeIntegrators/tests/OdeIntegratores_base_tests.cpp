50254caaf8a94880e14e60b661bc4393056832db
Incardona Pietro <incardon@mpi-cbg.de>
2021-08-12 11:01:02 +0200

    OdeInt Abhinav test, uncommenting and making them works

--- src/OdeIntegrators/tests/OdeIntegratores_base_tests.cpp
+++ src/OdeIntegrators/tests/OdeIntegratores_base_tests.cpp
@@ -77,7 +77,9 @@ void sigmoid( const state_type &x , state_type &dxdt , const double t )
 }
 
 BOOST_AUTO_TEST_SUITE(odeInt_BASE_tests)
-BOOST_AUTO_TEST_CASE(odeint_base_test1) {
+
+BOOST_AUTO_TEST_CASE(odeint_base_test1) 
+{
         size_t edgeSemiSize = 40;
         const size_t sz[2] = {edgeSemiSize,edgeSemiSize };
         Box<2, double> box({ 0, 0 }, { 1.0, 1.0 });
@@ -145,7 +147,6 @@ BOOST_AUTO_TEST_CASE(odeint_base_test1) {
         {
             rk4.do_step(Exponential,x0,t,dt);
             OdeSol=x0;
-            Particles.write_frame("OdeSol",int(t/dt));
             t+=dt;
         }
 
@@ -164,213 +165,209 @@ BOOST_AUTO_TEST_CASE(odeint_base_test1) {
         BOOST_REQUIRE_EQUAL(worst,worst2);
 }
 
-    BOOST_AUTO_TEST_CASE(odeint_base_test_STRUCT_ofp) {
-        size_t edgeSemiSize = 40;
-        const size_t sz[2] = {edgeSemiSize,edgeSemiSize };
-        Box<2, double> box({ 0, 0 }, { 1.0, 1.0 });
-        size_t bc[2] = { NON_PERIODIC, NON_PERIODIC };
-        double spacing[2];
-        spacing[0] = 1.0 / (sz[0] - 1);
-        spacing[1] = 1.0 / (sz[1] - 1);
-        double rCut = 3.9 * spacing[0];
-        Ghost<2, double> ghost(rCut);
-        BOOST_TEST_MESSAGE("Init vector_dist...");
+BOOST_AUTO_TEST_CASE(odeint_base_test_STRUCT_ofp) 
+{
+    size_t edgeSemiSize = 40;
+    const size_t sz[2] = {edgeSemiSize,edgeSemiSize };
+    Box<2, double> box({ 0, 0 }, { 1.0, 1.0 });
+    size_t bc[2] = { NON_PERIODIC, NON_PERIODIC };
+    double spacing[2];
+    spacing[0] = 1.0 / (sz[0] - 1);
+    spacing[1] = 1.0 / (sz[1] - 1);
+    double rCut = 3.9 * spacing[0];
+    Ghost<2, double> ghost(rCut);
+    BOOST_TEST_MESSAGE("Init vector_dist...");
 
-        vector_dist<2, double, aggregate<double,double,double,double,double,double>> Particles(0, box, bc, ghost);
+    vector_dist<2, double, aggregate<double,double,double,double,double,double>> Particles(0, box, bc, ghost);
 
-        auto it = Particles.getGridIterator(sz);
-        while (it.isNext())
-        {
-            Particles.add();
-            auto key = it.get();
-            mem_id k0 = key.get(0);
-            double xp0 = k0 * spacing[0];
-            Particles.getLastPos()[0] = xp0;
-            mem_id k1 = key.get(1);
-            double yp0 = k1 * spacing[1];
-            Particles.getLastPos()[1] = yp0;
-            Particles.getLastProp<0>() = xp0*yp0*exp(0);
-            Particles.getLastProp<1>() = xp0*yp0*exp(0.4);
-            Particles.getLastProp<2>() = xp0*yp0*exp(0);
-            Particles.getLastProp<3>() = xp0*yp0*exp(0.8);
-            ++it;
+    auto it = Particles.getGridIterator(sz);
+    while (it.isNext())
+    {
+        Particles.add();
+        auto key = it.get();
+        mem_id k0 = key.get(0);
+        double xp0 = k0 * spacing[0];
+        Particles.getLastPos()[0] = xp0;
+        mem_id k1 = key.get(1);
+        double yp0 = k1 * spacing[1];
+        Particles.getLastPos()[1] = yp0;
+        Particles.getLastProp<0>() = xp0*yp0*exp(0);
+        Particles.getLastProp<1>() = xp0*yp0*exp(0.4);
+        Particles.getLastProp<2>() = xp0*yp0*exp(0);
+        Particles.getLastProp<3>() = xp0*yp0*exp(0.8);
+        ++it;
+    }
+    Particles.map();
+    Particles.ghost_get<0>();
+    auto Init1 = getV<0>(Particles);
+    auto Sol1 = getV<1>(Particles);
+    auto Init2 = getV<2>(Particles);
+    auto Sol2 = getV<3>(Particles);
+    auto OdeSol1 = getV<4>(Particles);
+    auto OdeSol2 = getV<5>(Particles);
+
+    state_type_3d_ofp x0;//(Init1,Init2);
+    x0.data.get<0>()=Init1;
+    x0.data.get<1>()=Init2;
+    x0.data.get<2>()=Init1;
+
+    // The rhs of x' = f(x)
+    double t=0,tf=0.4;
+    const double dt=0.1;
+
+    //size_t steps=boost::numeric::odeint::integrate(Exponential_struct,x0,0.0,tf,dt);
+
+    //size_t steps=boost::numeric::odeint::integrate_const(
+    //        boost::numeric::odeint::runge_kutta4< state_type_struct_ofp<vector_type>,double,state_type_struct_ofp<vector_type>,double,boost::numeric::odeint::vector_space_algebra_ofp > ()
+    //                ,Exponential_struct_ofp,x0,0.0,tf,dt);
+
+    //typedef boost::numeric::odeint::controlled_runge_kutta< boost::numeric::odeint::runge_kutta_cash_karp54< state_type_struct_ofp<vector_type>,double,state_type_struct_ofp<vector_type>,double,boost::numeric::odeint::vector_space_algebra_ofp>> stepper_type;
+    typedef boost::numeric::odeint::controlled_runge_kutta< boost::numeric::odeint::runge_kutta_cash_karp54< state_type_3d_ofp,double,state_type_3d_ofp,double,boost::numeric::odeint::vector_space_algebra_ofp>> stepper_type;
+    integrate_adaptive( stepper_type() , Exponential_struct_ofp2 , x0 , t , tf , dt);
+
+    OdeSol1=x0.data.get<0>();
+    OdeSol2=x0.data.get<1>();
+    auto it2 = Particles.getDomainIterator();
+    double worst = 0.0;
+    double worst2 = 0.0;
+    while (it2.isNext()) {
+        auto p = it2.get();
+        if (fabs(Particles.getProp<1>(p) - Particles.getProp<4>(p)) > worst) {
+            worst = fabs(Particles.getProp<1>(p) - Particles.getProp<4>(p));
         }
-        Particles.map();
-        Particles.ghost_get<0>();
-        auto Init1 = getV<0>(Particles);
-        auto Sol1 = getV<1>(Particles);
-        auto Init2 = getV<2>(Particles);
-        auto Sol2 = getV<3>(Particles);
-        auto OdeSol1 = getV<4>(Particles);
-        auto OdeSol2 = getV<5>(Particles);
-
-        state_type_3d_ofp x0;//(Init1,Init2);
-        x0.data.get<0>()=Init1;
-        x0.data.get<1>()=Init2;
-        x0.data.get<2>()=Init1;
-
-        // The rhs of x' = f(x)
-        double t=0,tf=0.4;
-        const double dt=0.1;
-
-        //size_t steps=boost::numeric::odeint::integrate(Exponential_struct,x0,0.0,tf,dt);
-
-        //size_t steps=boost::numeric::odeint::integrate_const(
-        //        boost::numeric::odeint::runge_kutta4< state_type_struct_ofp<vector_type>,double,state_type_struct_ofp<vector_type>,double,boost::numeric::odeint::vector_space_algebra_ofp > ()
-        //                ,Exponential_struct_ofp,x0,0.0,tf,dt);
-
-        //typedef boost::numeric::odeint::controlled_runge_kutta< boost::numeric::odeint::runge_kutta_cash_karp54< state_type_struct_ofp<vector_type>,double,state_type_struct_ofp<vector_type>,double,boost::numeric::odeint::vector_space_algebra_ofp>> stepper_type;
-        typedef boost::numeric::odeint::controlled_runge_kutta< boost::numeric::odeint::runge_kutta_cash_karp54< state_type_3d_ofp,double,state_type_3d_ofp,double,boost::numeric::odeint::vector_space_algebra_ofp>> stepper_type;
-        integrate_adaptive( stepper_type() , Exponential_struct_ofp2 , x0 , t , tf , dt);
-
-        OdeSol1=x0.data.get<0>();
-        OdeSol2=x0.data.get<1>();
-        auto it2 = Particles.getDomainIterator();
-        double worst = 0.0;
-        double worst2 = 0.0;
-        while (it2.isNext()) {
-            auto p = it2.get();
-            if (fabs(Particles.getProp<1>(p) - Particles.getProp<4>(p)) > worst) {
-                worst = fabs(Particles.getProp<1>(p) - Particles.getProp<4>(p));
-            }
-            if (fabs(Particles.getProp<3>(p) - Particles.getProp<5>(p)) > worst2) {
-                worst2 = fabs(Particles.getProp<3>(p) - Particles.getProp<5>(p));
-            }
-            ++it2;
+        if (fabs(Particles.getProp<3>(p) - Particles.getProp<5>(p)) > worst2) {
+            worst2 = fabs(Particles.getProp<3>(p) - Particles.getProp<5>(p));
         }
-
-        BOOST_REQUIRE(worst < 1e-6);
-        BOOST_REQUIRE(worst2 < 1e-6);
-
-        //BOOST_REQUIRE(worst < 1e-6);
-
-/*        x0.u=Init1;
-        x0.v=Init2;
-
-        //x0=Init;
-        boost::numeric::odeint::runge_kutta4< state_type_struct_ofp > rk4;
-        while (t<tf)
-        {
-            rk4.do_step(Exponential_struct,x0,t,dt);
-            OdeSol1=x0.u;
-            OdeSol2=x0.v;
-            Particles.write_frame("OdeSol",int(t/dt));
-            t+=dt;
-        }
-
-        OdeSol1=x0.u;
-        OdeSol2=x0.v;
-        auto it3 = Particles.getDomainIterator();
-        double worst3 = 0.0;
-        double worst4 = 0.0;
-        while (it3.isNext()) {
-            auto p = it3.get();
-            if (fabs(Particles.getProp<1>(p) - Particles.getProp<4>(p)) > worst3) {
-                worst3 = fabs(Particles.getProp<1>(p) - Particles.getProp<4>(p));
-            }
-            if (fabs(Particles.getProp<3>(p) - Particles.getProp<5>(p)) > worst4) {
-                worst4 = fabs(Particles.getProp<3>(p) - Particles.getProp<5>(p));
-            }
-            ++it3;
-        }
-        std::cout<<worst3<<std::endl;
-        std::cout<<worst4<<std::endl;*/
-        //BOOST_REQUIRE(worst < 1e-6);
-        //BOOST_REQUIRE_EQUAL(worst,worst2);
+        ++it2;
     }
 
+    BOOST_REQUIRE(worst < 1e-6);
+    BOOST_REQUIRE(worst2 < 1e-6);
 
 
+    x0.data.get<0>()=Init1;
+    x0.data.get<1>()=Init2;
+    x0.data.get<2>()=Init1;
 
-
-    BOOST_AUTO_TEST_CASE(odeint_base_test2) {
-        size_t edgeSemiSize = 40;
-        const size_t sz[2] = {edgeSemiSize,edgeSemiSize };
-        Box<2, double> box({ 0, 0 }, { 1.0, 1.0 });
-        size_t bc[2] = { NON_PERIODIC, NON_PERIODIC };
-        double spacing[2];
-        spacing[0] = 1.0 / (sz[0] - 1);
-        spacing[1] = 1.0 / (sz[1] - 1);
-        double rCut = 3.9 * spacing[0];
-        Ghost<2, double> ghost(rCut);
-        BOOST_TEST_MESSAGE("Init vector_dist...");
-
-        vector_dist<2, double, aggregate<double, double,double>> Particles(0, box, bc, ghost);
-
-        double t=0.0,tf=0.5;
-        const double dt=0.1;
-
-        auto it = Particles.getGridIterator(sz);
-        while (it.isNext())
-        {
-            Particles.add();
-            auto key = it.get();
-            mem_id k0 = key.get(0);
-            double xp0 = k0 * spacing[0];
-            Particles.getLastPos()[0] = xp0;
-            mem_id k1 = key.get(1);
-            double yp0 = k1 * spacing[1];
-            Particles.getLastPos()[1] = yp0;
-            Particles.getLastProp<0>() = xp0*yp0*exp(t);
-            Particles.getLastProp<1>() = xp0*yp0*exp(tf);
-            ++it;
-        }
-        Particles.map();
-        Particles.ghost_get<0>();
-        auto Init = getV<0>(Particles);
-        auto Sol = getV<1>(Particles);
-        auto OdeSol = getV<2>(Particles);
-
-        state_type x0;
-        x0=Init;
-        // The rhs of x' = f(x)
-
-
-
-
-        //This doesnt work Why?
-        //size_t steps=boost::numeric::odeint::integrate(Exponential,x0,0.0,tf,dt);
-        typedef boost::numeric::odeint::controlled_runge_kutta< boost::numeric::odeint::runge_kutta_cash_karp54< state_type > > stepper_type;
-        integrate_adaptive( stepper_type() , Exponential , x0 , t , tf , dt);
-        //size_t steps=boost::numeric::odeint::integrate_const( boost::numeric::odeint::runge_kutta4< state_type >(),Exponential,x0,0.0,tf,dt);
-
-        OdeSol=x0;
-        auto it2 = Particles.getDomainIterator();
-        double worst = 0.0;
-        while (it2.isNext()) {
-            auto p = it2.get();
-            if (fabs(Particles.getProp<1>(p) - Particles.getProp<2>(p)) > worst) {
-                worst = fabs(Particles.getProp<1>(p) - Particles.getProp<2>(p));
-            }
-            ++it2;
-        }
-
-        BOOST_REQUIRE(worst < 1e-6);
-/*
-        x0=Init;
-        boost::numeric::odeint::runge_kutta4< state_type > rk4;
-        for( size_t i=0 ; i<int(tf/dt) ; ++i,t+=dt )
-        {
-            rk4.do_step(Exponential,x0,t,dt);
-            OdeSol=x0;
-            Particles.write_frame("OdeSol",int(t/dt));
-            t+=dt;
-        }
-
-        OdeSol=x0;
-        auto it3 = Particles.getDomainIterator();
-        double worst2 = 0.0;
-        while (it3.isNext()) {
-            auto p = it3.get();
-            if (fabs(Particles.getProp<1>(p) - Particles.getProp<2>(p)) > worst2) {
-                worst2 = fabs(Particles.getProp<1>(p) - Particles.getProp<2>(p));
-            }
-            ++it3;
-        }
-        //std::cout<<worst2<<std::endl;
-        BOOST_REQUIRE(worst < 1e-6);
-        BOOST_REQUIRE_EQUAL(worst,worst2);*/
+    //x0=Init;
+    boost::numeric::odeint::runge_kutta4< state_type_3d_ofp, double,state_type_3d_ofp,double,boost::numeric::odeint::vector_space_algebra_ofp> rk4;
+    while (t<tf)
+    {
+        rk4.do_step(Exponential_struct_ofp2,x0,t,dt);
+        t+=dt;
     }
+
+    OdeSol1=x0.data.get<0>();
+    OdeSol2=x0.data.get<1>();
+    auto it3 = Particles.getDomainIterator();
+    double worst3 = 0.0;
+    double worst4 = 0.0;
+    while (it3.isNext()) {
+        auto p = it3.get();
+        if (fabs(Particles.getProp<1>(p) - Particles.getProp<4>(p)) > worst3) {
+            worst3 = fabs(Particles.getProp<1>(p) - Particles.getProp<4>(p));
+        }
+        if (fabs(Particles.getProp<3>(p) - Particles.getProp<5>(p)) > worst4) {
+            worst4 = fabs(Particles.getProp<3>(p) - Particles.getProp<5>(p));
+        }
+        ++it3;
+    }
+
+    BOOST_REQUIRE(worst3 < 1e-6);
+    BOOST_REQUIRE(worst4 < 5e-5);
+}
+
+
+BOOST_AUTO_TEST_CASE(odeint_base_test2) 
+{
+    size_t edgeSemiSize = 40;
+    const size_t sz[2] = {edgeSemiSize,edgeSemiSize };
+    Box<2, double> box({ 0, 0 }, { 1.0, 1.0 });
+    size_t bc[2] = { NON_PERIODIC, NON_PERIODIC };
+    double spacing[2];
+    spacing[0] = 1.0 / (sz[0] - 1);
+    spacing[1] = 1.0 / (sz[1] - 1);
+    double rCut = 3.9 * spacing[0];
+    Ghost<2, double> ghost(rCut);
+    BOOST_TEST_MESSAGE("Init vector_dist...");
+
+    vector_dist<2, double, aggregate<double, double,double>> Particles(0, box, bc, ghost);
+
+    double t=0.0,tf=0.5;
+    const double dt=0.1;
+
+    auto it = Particles.getGridIterator(sz);
+    while (it.isNext())
+    {
+        Particles.add();
+        auto key = it.get();
+        mem_id k0 = key.get(0);
+        double xp0 = k0 * spacing[0];
+        Particles.getLastPos()[0] = xp0;
+        mem_id k1 = key.get(1);
+        double yp0 = k1 * spacing[1];
+        Particles.getLastPos()[1] = yp0;
+        Particles.getLastProp<0>() = xp0*yp0*exp(t);
+        Particles.getLastProp<1>() = xp0*yp0*exp(tf);
+        ++it;
+    }
+    Particles.map();
+    Particles.ghost_get<0>();
+    auto Init = getV<0>(Particles);
+    auto Sol = getV<1>(Particles);
+    auto OdeSol = getV<2>(Particles);
+
+    state_type x0;
+    x0=Init;
+    // The rhs of x' = f(x)
+
+
+
+
+    //This doesnt work Why?
+    //size_t steps=boost::numeric::odeint::integrate(Exponential,x0,0.0,tf,dt);
+    typedef boost::numeric::odeint::controlled_runge_kutta< boost::numeric::odeint::runge_kutta_cash_karp54< state_type > > stepper_type;
+    integrate_adaptive( stepper_type() , Exponential , x0 , t , tf , dt);
+    //size_t steps=boost::numeric::odeint::integrate_const( boost::numeric::odeint::runge_kutta4< state_type >(),Exponential,x0,0.0,tf,dt);
+
+    OdeSol=x0;
+    auto it2 = Particles.getDomainIterator();
+    double worst = 0.0;
+    while (it2.isNext()) {
+        auto p = it2.get();
+        if (fabs(Particles.getProp<1>(p) - Particles.getProp<2>(p)) > worst) {
+            worst = fabs(Particles.getProp<1>(p) - Particles.getProp<2>(p));
+        }
+        ++it2;
+    }
+
+    BOOST_REQUIRE(worst < 1e-6);
+
+/*    x0=Init;
+    boost::numeric::odeint::runge_kutta4< state_type > rk4;
+    for( size_t i=0 ; i<int(tf/dt) ; ++i,t+=dt )
+    {
+        rk4.do_step(Exponential,x0,t,dt);
+        OdeSol=x0;
+        Particles.write_frame("OdeSol",int(t/dt));
+        t+=dt;
+    }
+
+    OdeSol=x0;
+    auto it3 = Particles.getDomainIterator();
+    double worst2 = 0.0;
+    while (it3.isNext()) {
+        auto p = it3.get();
+        if (fabs(Particles.getProp<1>(p) - Particles.getProp<2>(p)) > worst2) {
+            worst2 = fabs(Particles.getProp<1>(p) - Particles.getProp<2>(p));
+        }
+        ++it3;
+    }
+    //std::cout<<worst2<<std::endl;
+    BOOST_REQUIRE(worst < 1e-6);
+    BOOST_REQUIRE_EQUAL(worst,worst2);*/
+}
+
     BOOST_AUTO_TEST_CASE(odeint_base_test3) {
         size_t edgeSemiSize = 40;
         const size_t sz[2] = {edgeSemiSize,edgeSemiSize };
