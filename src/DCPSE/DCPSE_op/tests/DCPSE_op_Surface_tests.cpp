1c44c07533ee6c0b3fd9980d99e62afc5fd94ed9
absingh <absingh@mpi-cbg.de>
2021-12-07 14:01:01 +0530

    Fixing Tests

--- src/DCPSE/DCPSE_op/tests/DCPSE_op_Surface_tests.cpp
+++ src/DCPSE/DCPSE_op/tests/DCPSE_op_Surface_tests.cpp
@@ -16,7 +16,7 @@
 #include <boost/test/unit_test.hpp>
 #include <iostream>
 #include "../DCPSE_surface_op.hpp"
-//#include "../DCPSE_Solver.hpp"
+#include "../DCPSE_Solver.hpp"
 #include "Operators/Vector/vector_dist_operators.hpp"
 #include "Vector/vector_dist_subset.hpp"
 #include <iostream>
@@ -96,9 +96,7 @@ BOOST_AUTO_TEST_SUITE(dcpse_op_suite_tests)
 
         double boxP1{-1.5}, boxP2{1.5};
         double boxSize{boxP2 - boxP1};
-        size_t n=1024;
-        std::cout<<"Enter Res:";
-        std::cin>>n;
+        size_t n=512;
         size_t sz[2] = {n,n};
         double grid_spacing{boxSize/(sz[0]-1)};
         double rCut{3.9 * grid_spacing};
@@ -177,6 +175,90 @@ BOOST_AUTO_TEST_SUITE(dcpse_op_suite_tests)
         std::cout<<worst;
         BOOST_REQUIRE(worst < 0.03);
 }
+    BOOST_AUTO_TEST_CASE(dcpse_surface_solver_circle) {
+        double boxP1{-1.5}, boxP2{1.5};
+        double boxSize{boxP2 - boxP1};
+        size_t n=512;
+        size_t sz[2] = {n,n};
+        double grid_spacing{boxSize/(sz[0]-1)};
+        double rCut{3.9 * grid_spacing};
+
+        Box<2,double> domain{{boxP1,boxP1},{boxP2,boxP2}};
+        size_t bc[2] = {NON_PERIODIC,NON_PERIODIC};
+        Ghost<2,double> ghost{rCut + grid_spacing/8.0};
+        auto &v_cl=create_vcluster();
+
+        vector_dist_ws<2, double, aggregate<double,double,double[2],double,double[2],double>> Sparticles(0, domain,bc,ghost);
+        //Init_DCPSE(domain)
+        BOOST_TEST_MESSAGE("Init domain...");
+
+        // Surface prameters
+        const double radius{1.0};
+        std::array<double,2> center{0.0,0.0};
+        Point<2,double> coord;
+        const double pi{3.14159265358979323846};
+
+        // 1. Particles on surface
+        double theta{0.0};
+        double dtheta{2*pi/double(n)};
+        if (v_cl.rank() == 0) {
+            for (int i = 0; i < n; ++i) {
+                coord[0] = center[0] + radius * std::cos(theta);
+                coord[1] = center[1] + radius * std::sin(theta);
+                Sparticles.add();
+                Sparticles.getLastPos()[0] = coord[0];
+                Sparticles.getLastPos()[1] = coord[1];
+                Sparticles.getLastProp<3>() = -4*std::sin(2*theta);
+                Sparticles.getLastProp<2>()[0] = std::cos(theta);
+                Sparticles.getLastProp<2>()[1] = std::sin(theta);
+                Sparticles.getLastProp<1>() = std::sin(2*theta);;//sin(theta)*exp(-finalT/(radius*radius));
+                Sparticles.getLastSubset(0);
+                if(coord[0]==1. && coord[1]==0.)
+                {Sparticles.getLastSubset(1);}
+                theta += dtheta;
+            }
+        }
+        Sparticles.map();
+
+        BOOST_TEST_MESSAGE("Sync domain across processors...");
+
+        Sparticles.ghost_get<0>();
+        Sparticles.write("Sparticles");
+        vector_dist_subset<2, double, aggregate<double,double,double[2],double,double[2],double>> Sparticles_bulk(Sparticles,0);
+        vector_dist_subset<2, double, aggregate<double,double,double[2],double,double[2],double>> Sparticles_boundary(Sparticles,1);
+        auto & bulk=Sparticles_bulk.getIds();
+        auto & boundary=Sparticles_boundary.getIds();
+        //Here template parameters are Normal property no.
+        SurfaceDerivative_xx<2> SDxx(Sparticles, 2, rCut,grid_spacing);
+        SurfaceDerivative_yy<2> SDyy(Sparticles, 2, rCut,grid_spacing);
+        auto INICONC = getV<3>(Sparticles);
+        auto CONC = getV<0>(Sparticles);
+        auto TEMP = getV<4>(Sparticles);
+        auto normal = getV<2>(Sparticles);
+        auto ANASOL = getV<1>(Sparticles);
+        DCPSE_scheme<equations2d1,decltype(Sparticles)> Solver(Sparticles);
+        Solver.impose(SDxx(CONC)+SDyy(CONC), bulk, INICONC);
+        Solver.impose(CONC, boundary, ANASOL);
+        Solver.solve(CONC);
+
+
+        auto it2 = Sparticles.getDomainIterator();
+        double worst = 0.0;
+        while (it2.isNext()) {
+            auto p = it2.get();
+            Sparticles.getProp<5>(p) = fabs(Sparticles.getProp<1>(p) - Sparticles.getProp<0>(p));
+            if (fabs(Sparticles.getProp<1>(p) - Sparticles.getProp<0>(p)) > worst) {
+                worst = fabs(Sparticles.getProp<1>(p) - Sparticles.getProp<0>(p));
+            }
+            ++it2;
+        }
+        Sparticles.deleteGhost();
+        Sparticles.write("Sparticles");
+        std::cout<<worst;
+        BOOST_REQUIRE(worst < 0.03);
+}
+
+
 BOOST_AUTO_TEST_SUITE_END()
 
 #endif
