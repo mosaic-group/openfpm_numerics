65dd2dabb2acb8381fae1ffaab440913a8106023
absingh <absingh@mpi-cbg.de>
2021-12-23 14:38:43 +0530

    Updaing Surface operators and tests

--- src/DCPSE/DCPSE_op/DCPSE_surface_op.hpp
+++ src/DCPSE/DCPSE_op/DCPSE_surface_op.hpp
@@ -217,6 +217,78 @@ public:
 };
 
 template<unsigned int NORMAL_ID>
+class Laplace_Beltrami {
+
+    void *dcpse;
+
+public:
+    /*! \brief Class for Creating the DCPSE Operator Dxx and objects and computs DCPSE Kernels.
+     *
+     *
+     * \param parts particle set
+     * \param ord order of convergence of the operator
+     * \param rCut Argument for cell list construction
+     * \param oversampling_factor multiplier to the minimum no. of particles required by the operator in support
+     * \param support_options default:N_particles, Radius can be used to select all particles inside rCut. Overrides oversampling.
+     *
+     * \return Operator Dxx which is a function on Vector_dist_Expressions
+     *
+     */
+    template<typename particles_type>
+    Laplace_Beltrami(particles_type &parts, unsigned int ord, typename particles_type::stype rCut,typename particles_type::stype nSpacing,
+                    support_options opt = support_options::RADIUS) {
+        Point<particles_type::dims, unsigned int> p;
+        p.zero();
+        p.get(0) = 2;
+        p.get(1) = 2;
+        p.get(2) = 2;
+
+        dcpse = new Dcpse<particles_type::dims, particles_type>(parts, p, ord, rCut,nSpacing,value_t<NORMAL_ID>(), opt);
+    }
+
+    template<typename particles_type>
+    void deallocate(particles_type &parts) {
+        delete (Dcpse<particles_type::dims, particles_type> *) dcpse;
+    }
+
+    template<typename operand_type>
+    vector_dist_expression_op<operand_type, Dcpse<operand_type::vtype::dims, typename operand_type::vtype>, VECT_DCPSE>
+    operator()(operand_type arg) {
+        typedef Dcpse<operand_type::vtype::dims, typename operand_type::vtype> dcpse_type;
+
+        return vector_dist_expression_op<operand_type, dcpse_type, VECT_DCPSE>(arg, *(dcpse_type *) dcpse);
+    }
+
+    template<typename particles_type>
+    void checkMomenta(particles_type &particles) {
+        auto dcpse_temp = (Dcpse<particles_type::dims, particles_type> *) dcpse;
+        dcpse_temp->checkMomenta(particles);
+
+    }
+
+    template<unsigned int prp, typename particles_type>
+    void DrawKernel(particles_type &particles, int k) {
+        auto dcpse_temp = (Dcpse<particles_type::dims, particles_type> *) dcpse;
+        dcpse_temp->template DrawKernel<prp>(particles, k);
+
+    }
+
+    /*! \brief Method for Updating the DCPSE Operator by recomputing DCPSE Kernels.
+     *
+     *
+     * \param parts particle set
+     */
+    template<typename particles_type>
+    void update(particles_type &particles) {
+        auto dcpse_temp = (Dcpse<particles_type::dims, particles_type> *) dcpse;
+        dcpse_temp->createNormalParticles<NORMAL_ID>(particles);
+        dcpse_temp->createNormalParticles<NORMAL_ID>(particles);
+        dcpse_temp->initializeUpdate(particles);
+        dcpse_temp->accumulateAndDeleteNormalParticles(particles);
+    }
+};
+
+template<unsigned int NORMAL_ID>
 class SurfaceDerivative_xx {
 
     void *dcpse;
@@ -420,7 +492,8 @@ public:
     template<typename particles_type>
     void update(particles_type &particles) {
         auto dcpse_temp = (Dcpse<particles_type::dims, particles_type> *) dcpse;
-        dcpse_temp->createNormalParticles<NORMAL_ID>(particles);
+        dcpse_temp->template createNormalParticles<NORMAL_ID>(particles);
+        particles.write("With Normal");
         dcpse_temp->initializeUpdate(particles);
         dcpse_temp->accumulateAndDeleteNormalParticles(particles);
 
--- src/DCPSE/DCPSE_op/tests/DCPSE_op_Surface_tests.cpp
+++ src/DCPSE/DCPSE_op/tests/DCPSE_op_Surface_tests.cpp
@@ -20,7 +20,7 @@
 #include "Operators/Vector/vector_dist_operators.hpp"
 #include "Vector/vector_dist_subset.hpp"
 #include <iostream>
-
+#include "util/SphericalHarmonics.hpp"
 
 BOOST_AUTO_TEST_SUITE(dcpse_op_suite_tests)
     BOOST_AUTO_TEST_CASE(dcpse_surface_simple) {
@@ -86,9 +86,9 @@ BOOST_AUTO_TEST_SUITE(dcpse_op_suite_tests)
             ++it2;
         }
         Sparticles.deleteGhost();
-        std::cout<<v_cl.rank()<<":WORST:"<<worst<<std::endl;
+        //std::cout<<v_cl.rank()<<":WORST:"<<worst<<std::endl;
         //Sparticles.write("Sparticles");
-        //BOOST_REQUIRE(worst < 0.03);
+        BOOST_REQUIRE(worst < 0.03);
 }
     BOOST_AUTO_TEST_CASE(dcpse_surface_circle) {
 
@@ -259,8 +259,267 @@ BOOST_AUTO_TEST_SUITE(dcpse_op_suite_tests)
         //std::cout<<worst;
         BOOST_REQUIRE(worst < 0.03);
 }
+BOOST_AUTO_TEST_CASE(dcpse_surface_sphere) {
+  auto & v_cl = create_vcluster();
+  timer tt;
+  tt.start();
+  size_t n=512;
+  size_t n_sp=n;
+  // Domain
+  double boxP1{-1.5}, boxP2{1.5};
+  double boxSize{boxP2 - boxP1};
+  size_t sz[3] = {n,n,n};
+  double grid_spacing{boxSize/(sz[0]-1)};
+  double grid_spacing_surf=grid_spacing*30;
+  double rCut{2.5 * grid_spacing_surf};
+
+  Box<3,double> domain{{boxP1,boxP1,boxP1},{boxP2,boxP2,boxP2}};
+  size_t bc[3] = {NON_PERIODIC,NON_PERIODIC,NON_PERIODIC};
+  Ghost<3,double> ghost{rCut + grid_spacing/8.0};
+
+  constexpr int K = 1;
+  // particles
+  vector_dist_ws<3, double, aggregate<double,double,double[3],double,double[3],double>> Sparticles(0, domain,bc,ghost);
+  // 1. particles on the Spherical surface
+  double Golden_angle=M_PI * (3.0 - sqrt(5.0));
+  if (v_cl.rank() == 0) {
+    //std::vector<Vector3f> data;
+    //GenerateSphere(1,data);
+    for(int i=1;i<n_sp;i++)
+        {
+            double y = 1.0 - (i /double(n_sp - 1.0)) * 2.0;
+            double radius = sqrt(1 - y * y);
+            double Golden_theta = Golden_angle * i;
+            double x = cos(Golden_theta) * radius;
+            double z = sin(Golden_theta) * radius;
+            Sparticles.add();
+            Sparticles.getLastPos()[0] = x;
+            Sparticles.getLastPos()[1] = y;
+            Sparticles.getLastPos()[2] = z;
+            double rm=sqrt(x*x+y*y+z*z);
+            Sparticles.getLastProp<2>()[0] = x/rm;
+            Sparticles.getLastProp<2>()[1] = y/rm;
+            Sparticles.getLastProp<2>()[2] = z/rm;
+            Sparticles.getLastProp<4>()[0] = 1.0 ;
+            Sparticles.getLastProp<4>()[1] = std::atan2(sqrt(x*x+y*y),z);
+            Sparticles.getLastProp<4>()[2] = std::atan2(y,x);
+            if(i<=2*(K)+1)
+            {Sparticles.getLastSubset(1);}
+            else
+            {Sparticles.getLastSubset(0);}
+        }
+    //std::cout << "n: " << n << " - grid spacing: " << grid_spacing << " - rCut: " << rCut << "Surf Normal spacing" << grid_spacing<<std::endl;
+  }
+
+  Sparticles.map();
+  Sparticles.ghost_get<3>();
+
+  vector_dist_subset<3,double,aggregate<double,double,double[3],double,double[3],double>> Sparticles_bulk(Sparticles,0);
+  vector_dist_subset<3,double,aggregate<double,double,double[3],double,double[3],double>> Sparticles_boundary(Sparticles,1);
+  auto &bulkIds=Sparticles_bulk.getIds();
+  auto &bdrIds=Sparticles_boundary.getIds();
+  std::unordered_map<const lm,double,key_hash,key_equal> Alm;
+  //Setting max mode l_max
+  //Setting amplitudes to 1
+  for(int l=0;l<=K;l++){
+      for(int m=-l;m<=l;m++){
+          Alm[std::make_tuple(l,m)]=0;
+      }
+  }
+  Alm[std::make_tuple(1,0)]=1;
+  auto it2 = Sparticles.getDomainIterator();
+  while (it2.isNext()) {
+      auto p = it2.get();
+      Point<3, double> xP = Sparticles.getProp<4>(p);
+      /*double Sum=0;
+      for(int m=-spL;m<=spL;++m)
+      {
+        Sum+=openfpm::math::Y(spL,m,xP[1],xP[2]);
+      }*/
+      //Sparticles.getProp<ANADF>(p) = Sum;//openfpm::math::Y(K,K,xP[1],xP[2]);openfpm::math::sumY_Scalar<K>(xP[0],xP[1],xP[2],Alm);;
+      Sparticles.getProp<3>(p)=openfpm::math::sumY_Scalar<K>(xP[0],xP[1],xP[2],Alm);
+      Sparticles.getProp<1>(p)=-(K)*(K+1)*openfpm::math::sumY_Scalar<K>(xP[0],xP[1],xP[2],Alm);
+      ++it2;
+  }
+  auto f=getV<3>(Sparticles);
+  auto Df=getV<0>(Sparticles);
+
+  SurfaceDerivative_xx<2> Sdxx{Sparticles,2,rCut,grid_spacing_surf};
+  SurfaceDerivative_yy<2> Sdyy{Sparticles,2,rCut,grid_spacing_surf};
+  SurfaceDerivative_zz<2> Sdzz{Sparticles,2,rCut,grid_spacing_surf};
+  //Laplace_Beltrami<2> SLap{Sparticles,2,rCut,grid_spacing_surf};
+  Sdxx.DrawKernel<5>(Sparticles,0);
+  //Sdyy.DrawKernel<5>(Sparticles,0);
+  //Sdzz.DrawKernel<5>(Sparticles,0);
+/*  std::cout<<"SDXX:"<<std::endl;
+  Sdxx.checkMomenta(Sparticles);
+  std::cout<<"SDYY:"<<std::endl;
+  Sdyy.checkMomenta(Sparticles);
+  std::cout<<"SDZZ:"<<std::endl;
+  Sdzz.checkMomenta(Sparticles);*/
 
 
+  Df=(Sdxx(f)+Sdyy(f)+Sdzz(f));
+  //Df=SLap(f);
+  auto it3 = Sparticles.getDomainIterator();
+  double worst = 0.0;
+  while (it3.isNext()) {
+      auto p = it3.get();
+      //Sparticles.getProp<5>(p) = fabs(Sparticles.getProp<1>(p) - Sparticles.getProp<0>(p));
+      if (fabs(Sparticles.getProp<1>(p) - Sparticles.getProp<0>(p)) > worst) {
+          worst = fabs(Sparticles.getProp<1>(p) - Sparticles.getProp<0>(p));
+      }
+      ++it3;
+  }
+        Sparticles.deleteGhost();
+        //Sparticles.write("Sparticles");
+        //std::cout<<worst;
+        BOOST_REQUIRE(worst < 0.03);
+}
+
+
+BOOST_AUTO_TEST_CASE(dcpse_surface_sphere_old) {
+  auto & v_cl = create_vcluster();
+  timer tt;
+  tt.start();
+  size_t n=512;
+  size_t n_sp=n;
+  // Domain
+  double boxP1{-1.5}, boxP2{1.5};
+  double boxSize{boxP2 - boxP1};
+  size_t sz[3] = {n,n,n};
+  double grid_spacing{boxSize/(sz[0]-1)};
+  double grid_spacing_surf=grid_spacing*30;
+  double rCut{2.5 * grid_spacing_surf};
+
+  Box<3,double> domain{{boxP1,boxP1,boxP1},{boxP2,boxP2,boxP2}};
+  size_t bc[3] = {NON_PERIODIC,NON_PERIODIC,NON_PERIODIC};
+  Ghost<3,double> ghost{rCut + grid_spacing/8.0};
+
+  constexpr int K = 1;
+  // particles
+  vector_dist_ws<3, double, aggregate<double,double,double[3],double,double[3],double>> Sparticles(0, domain,bc,ghost);
+  // 1. particles on the Spherical surface
+  double Golden_angle=M_PI * (3.0 - sqrt(5.0));
+  if (v_cl.rank() == 0) {
+    //std::vector<Vector3f> data;
+    //GenerateSphere(1,data);
+    std::unordered_map<const lm,double,key_hash,key_equal> Alm;
+          //Setting max mode l_max
+          //Setting amplitudes to 1
+          for(int l=0;l<=K;l++){
+              for(int m=-l;m<=l;m++){
+                  Alm[std::make_tuple(l,m)]=0;
+              }
+          }
+    Alm[std::make_tuple(1,0)]=1;
+    for(int i=1;i<n_sp;i++)
+        {
+            double y = 1.0 - (i /double(n_sp - 1.0)) * 2.0;
+            double radius = sqrt(1 - y * y);
+            double Golden_theta = Golden_angle * i;
+            double x = cos(Golden_theta) * radius;
+            double z = sin(Golden_theta) * radius;
+            Sparticles.add();
+            Sparticles.getLastPos()[0] = x;
+            Sparticles.getLastPos()[1] = y;
+            Sparticles.getLastPos()[2] = z;
+            double rm=sqrt(x*x+y*y+z*z);
+            Sparticles.getLastProp<2>()[0] = x/rm;
+            Sparticles.getLastProp<2>()[1] = y/rm;
+            Sparticles.getLastProp<2>()[2] = z/rm;
+            Sparticles.getLastProp<4>()[0] = 1.0 ;
+            Sparticles.getLastProp<4>()[1] = std::atan2(sqrt(x*x+y*y),z);
+            Sparticles.getLastProp<4>()[2] = std::atan2(y,x);
+            double m1=openfpm::math::sumY_Scalar<K>(1.0,std::atan2(sqrt(x*x+y*y),z),std::atan2(y,x),Alm);
+            double m2=-(K)*(K+1)*openfpm::math::sumY_Scalar<K>(1.0,std::atan2(sqrt(x*x+y*y),z),std::atan2(y,x),Alm);
+            Sparticles.getLastProp<3>()=m1;
+            Sparticles.getLastProp<1>()=m2;
+            Sparticles.getLastSubset(0);
+            for(int j=1;j<=2;++j){
+                Sparticles.add();
+            Sparticles.getLastPos()[0] = x+j*grid_spacing_surf*x/rm;
+            Sparticles.getLastPos()[1] = y+j*grid_spacing_surf*y/rm;
+            Sparticles.getLastPos()[2] = z+j*grid_spacing_surf*z/rm;
+            Sparticles.getLastProp<3>()=m1;
+            Sparticles.getLastSubset(1);
+            //Sparticles.getLastProp<1>(p)=m2;
+            Sparticles.add();
+            Sparticles.getLastPos()[0] = x-j*grid_spacing_surf*x/rm;
+            Sparticles.getLastPos()[1] = y-j*grid_spacing_surf*y/rm;
+            Sparticles.getLastPos()[2] = z-j*grid_spacing_surf*z/rm;
+            Sparticles.getLastProp<3>()=m1;
+            Sparticles.getLastSubset(1);
+            //Sparticles.getLastProp<1>(p)=m2;
+            }
+        }
+    //std::cout << "n: " << n << " - grid spacing: " << grid_spacing << " - rCut: " << rCut << "Surf Normal spacing" << grid_spacing<<std::endl;
+  }
+
+  Sparticles.map();
+  Sparticles.ghost_get<3>();
+  //Sparticles.write("SparticlesInit");
+
+  vector_dist_subset<3,double,aggregate<double,double,double[3],double,double[3],double>> Sparticles_bulk(Sparticles,0);
+  vector_dist_subset<3,double,aggregate<double,double,double[3],double,double[3],double>> Sparticles_boundary(Sparticles,1);
+  auto &bulkIds=Sparticles_bulk.getIds();
+  auto &bdrIds=Sparticles_boundary.getIds();
+  /*auto it2 = Sparticles.getDomainIterator();
+  while (it2.isNext()) {
+      auto p = it2.get();
+      Point<3, double> xP = Sparticles.getProp<4>(p);
+      *//*double Sum=0;
+      for(int m=-spL;m<=spL;++m)
+      {
+        Sum+=openfpm::math::Y(spL,m,xP[1],xP[2]);
+      }*//*
+      //Sparticles.getProp<ANADF>(p) = Sum;//openfpm::math::Y(K,K,xP[1],xP[2]);openfpm::math::sumY_Scalar<K>(xP[0],xP[1],xP[2],Alm);;
+      Sparticles.getProp<3>(p)=openfpm::math::sumY_Scalar<K>(xP[0],xP[1],xP[2],Alm);
+      Sparticles.getProp<1>(p)=-(K)*(K+1)*openfpm::math::sumY_Scalar<K>(xP[0],xP[1],xP[2],Alm);
+      ++it2;
+  }*/
+  auto f=getV<3>(Sparticles);
+  auto Df=getV<0>(Sparticles);
+
+  //SurfaceDerivative_xx<2> Sdxx{Sparticles,2,rCut,grid_spacing_surf};
+  //SurfaceDerivative_yy<2> Sdyy{Sparticles,2,rCut,grid_spacing_surf};
+  //SurfaceDerivative_zz<2> Sdzz{Sparticles,2,rCut,grid_spacing_surf};
+  Derivative_xx Sdxx{Sparticles,2,rCut};
+  //std::cout<<"Dxx Done"<<std::endl;
+  Derivative_yy Sdyy{Sparticles,2,rCut};
+  //std::cout<<"Dyy Done"<<std::endl;
+  Derivative_zz Sdzz{Sparticles,2,rCut};
+  //std::cout<<"Dzz Done"<<std::endl;
+
+  //Laplace_Beltrami<2> SLap{Sparticles,2,rCut,grid_spacing_surf};
+  //SLap.DrawKernel<5>(Sparticles,73);
+  Sdxx.DrawKernel<5>(Sparticles,0);
+  //Sdyy.DrawKernel<5>(Sparticles,0);
+  //Sdzz.DrawKernel<5>(Sparticles,0);
+/*  std::cout<<"SDXX:"<<std::endl;
+  Sdxx.checkMomenta(Sparticles);
+  std::cout<<"SDYY:"<<std::endl;
+  Sdyy.checkMomenta(Sparticles);
+  std::cout<<"SDZZ:"<<std::endl;
+  Sdzz.checkMomenta(Sparticles);*/
+
+
+  Df=(Sdxx(f)+Sdyy(f)+Sdzz(f));
+  //Df=SLap(f);
+  //auto it3 = Sparticles_bulk.getDomainIterator();
+  double worst = 0.0;
+  for (int j = 0; j < bulkIds.size(); j++) {
+      auto p = bulkIds.get<0>(j);
+      //Sparticles.getProp<5>(p) = fabs(Sparticles.getProp<1>(p) - Sparticles.getProp<0>(p));
+        if (fabs(Sparticles.getProp<1>(p) - Sparticles.getProp<0>(p)) > worst) {
+                  worst = fabs(Sparticles.getProp<1>(p) - Sparticles.getProp<0>(p));
+              }
+  }
+        Sparticles.deleteGhost();
+        //Sparticles.write("SparticlesNoo");
+        //std::cout<<"Worst: "<<worst<<std::endl;
+        BOOST_REQUIRE(worst < 0.03);
+}
 BOOST_AUTO_TEST_SUITE_END()
 
 #endif
