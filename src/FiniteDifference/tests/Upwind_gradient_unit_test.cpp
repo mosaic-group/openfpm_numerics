98f286f2ae05cf8e2e23d69360108bb665f2ea97
jstark <jstark@mpi-cbg.de>
2022-08-09 11:59:26 +0200

    Added vector type velocity field option to upwind gradient.

--- src/FiniteDifference/tests/Upwind_gradient_unit_test.cpp
+++ src/FiniteDifference/tests/Upwind_gradient_unit_test.cpp
@@ -13,11 +13,11 @@
 
 BOOST_AUTO_TEST_SUITE(UpwindGradientTestSuite)
 	const double EPSILON = 1e-14;
-	const size_t f_gaussian   = 0;
-	const size_t Sign         = 1;
-	const size_t df_gaussian  = 2;
-	const size_t df_upwind    = 3;
-	const size_t Error        = 4;
+	const size_t F_GAUSSIAN   = 0;
+	const size_t VELOCITY         = 1;
+	const size_t dF_GAUSSIAN  = 2;
+	const size_t dF_UPWIND    = 3;
+	const size_t ERROR        = 4;
 	
 	BOOST_AUTO_TEST_CASE(Upwind_gradient_order1_1D_test)
 	{
@@ -40,7 +40,7 @@ BOOST_AUTO_TEST_SUITE(UpwindGradientTestSuite)
 			const size_t sz[grid_dim] = {N};
 			grid_in_type g_dist(sz, box, ghost);
 			
-			g_dist.setPropNames({"f_gaussian", "Sign", "df_gaussian", "df_upwind", "Error"});
+			g_dist.setPropNames({"F_GAUSSIAN", "VELOCITY", "dF_GAUSSIAN", "dF_UPWIND", "ERROR"});
 			
 			auto gdom = g_dist.getDomainGhostIterator();
 			while (gdom.isNext())
@@ -48,9 +48,9 @@ BOOST_AUTO_TEST_SUITE(UpwindGradientTestSuite)
 				auto key = gdom.get();
 				Point<grid_dim, double> p = g_dist.getPos(key);
 				// Initialize grid and ghost with gaussian function
-				g_dist.getProp<f_gaussian>(key) = gaussian(p, mu, sigma);
+				g_dist.getProp<F_GAUSSIAN>(key) = gaussian(p, mu, sigma);
 				// Initialize the sign of the gaussian function
-				g_dist.getProp<Sign>(key) = sgn(g_dist.getProp<f_gaussian>(key));
+				g_dist.getProp<VELOCITY>(key) = sgn(g_dist.getProp<F_GAUSSIAN>(key));
 				++gdom;
 			}
 			
@@ -63,19 +63,19 @@ BOOST_AUTO_TEST_SUITE(UpwindGradientTestSuite)
 				for (int d = 0; d < grid_dim; d++)
 				{
 					// Analytical gradient
-					g_dist.getProp<df_gaussian>(key)[d] = hermite_polynomial(p.get(d), sigma, 1) * g_dist.getProp<f_gaussian>(key);
+					g_dist.getProp<dF_GAUSSIAN>(key)[d] = hermite_polynomial(p.get(d), sigma, 1) * g_dist.getProp<F_GAUSSIAN>(key);
 				}
 				++dom;
 			}
 			
 			// Upwind gradient with specific convergence order, not becoming one-sided at the boundary
-			get_upwind_gradient<f_gaussian, Sign, df_upwind>(g_dist, convergence_order, false);
+			get_upwind_gradient<F_GAUSSIAN, VELOCITY, dF_UPWIND>(g_dist, convergence_order, false);
 			
 			// Get the error between analytical and numerical solution
-			get_relative_error<df_upwind, df_gaussian, Error>(g_dist);
+			get_relative_error<dF_UPWIND, dF_GAUSSIAN, ERROR>(g_dist);
 			
 			LNorms<double> lNorms;
-			lNorms.get_l_norms_grid<Error>(g_dist);
+			lNorms.get_l_norms_grid<ERROR>(g_dist);
 			
 			if (N==32) BOOST_CHECK_MESSAGE(lNorms.l2 <= 0.38954184748195 + EPSILON, "Checking L2-norm upwind gradient "
 																				"order " + std::to_string
@@ -105,7 +105,7 @@ BOOST_AUTO_TEST_SUITE(UpwindGradientTestSuite)
 			const size_t sz[grid_dim] = {N};
 			grid_in_type g_dist(sz, box, ghost);
 			
-			g_dist.setPropNames({"f_gaussian", "Sign", "df_gaussian", "df_upwind", "Error"});
+			g_dist.setPropNames({"F_GAUSSIAN", "VELOCITY", "dF_GAUSSIAN", "dF_UPWIND", "ERROR"});
 			
 			auto gdom = g_dist.getDomainGhostIterator();
 			while (gdom.isNext())
@@ -113,9 +113,9 @@ BOOST_AUTO_TEST_SUITE(UpwindGradientTestSuite)
 				auto key = gdom.get();
 				Point<grid_dim, double> p = g_dist.getPos(key);
 				// Initialize grid and ghost with gaussian function
-				g_dist.getProp<f_gaussian>(key) = gaussian(p, mu, sigma);
+				g_dist.getProp<F_GAUSSIAN>(key) = gaussian(p, mu, sigma);
 				// Initialize the sign of the gaussian function
-				g_dist.getProp<Sign>(key) = sgn(g_dist.getProp<f_gaussian>(key));
+				g_dist.getProp<VELOCITY>(key) = sgn(g_dist.getProp<F_GAUSSIAN>(key));
 				++gdom;
 			}
 			
@@ -128,19 +128,19 @@ BOOST_AUTO_TEST_SUITE(UpwindGradientTestSuite)
 				for (int d = 0; d < grid_dim; d++)
 				{
 					// Analytical gradient
-					g_dist.getProp<df_gaussian>(key)[d] = hermite_polynomial(p.get(d), sigma, 1) * g_dist.getProp<f_gaussian>(key);
+					g_dist.getProp<dF_GAUSSIAN>(key)[d] = hermite_polynomial(p.get(d), sigma, 1) * g_dist.getProp<F_GAUSSIAN>(key);
 				}
 				++dom;
 			}
 			
 			// Upwind gradient with specific convergence order, not becoming one-sided at the boundary
-			get_upwind_gradient<f_gaussian, Sign, df_upwind>(g_dist, convergence_order, false);
+			get_upwind_gradient<F_GAUSSIAN, VELOCITY, dF_UPWIND>(g_dist, convergence_order, false);
 			
 			// Get the error between analytical and numerical solution
-			get_relative_error<df_upwind, df_gaussian, Error>(g_dist);
+			get_relative_error<dF_UPWIND, dF_GAUSSIAN, ERROR>(g_dist);
 			
 			LNorms<double> lNorms;
-			lNorms.get_l_norms_grid<Error>(g_dist);
+			lNorms.get_l_norms_grid<ERROR>(g_dist);
 			
 			if (N==32) BOOST_CHECK_MESSAGE(lNorms.l2 <= 0.08667855716144 + EPSILON, "Checking L2-norm upwind gradient "
 																				"order "
@@ -168,7 +168,7 @@ BOOST_AUTO_TEST_SUITE(UpwindGradientTestSuite)
 			const size_t sz[grid_dim] = {N};
 			grid_in_type g_dist(sz, box, ghost);
 			
-			g_dist.setPropNames({"f_gaussian", "Sign", "df_gaussian", "df_upwind", "Error"});
+			g_dist.setPropNames({"F_GAUSSIAN", "VELOCITY", "dF_GAUSSIAN", "dF_UPWIND", "ERROR"});
 			
 			auto gdom = g_dist.getDomainGhostIterator();
 			while (gdom.isNext())
@@ -176,9 +176,9 @@ BOOST_AUTO_TEST_SUITE(UpwindGradientTestSuite)
 				auto key = gdom.get();
 				Point<grid_dim, double> p = g_dist.getPos(key);
 				// Initialize grid and ghost with gaussian function
-				g_dist.getProp<f_gaussian>(key) = gaussian(p, mu, sigma);
+				g_dist.getProp<F_GAUSSIAN>(key) = gaussian(p, mu, sigma);
 				// Initialize the sign of the gaussian function
-				g_dist.getProp<Sign>(key) = sgn(g_dist.getProp<f_gaussian>(key));
+				g_dist.getProp<VELOCITY>(key) = sgn(g_dist.getProp<F_GAUSSIAN>(key));
 				++gdom;
 			}
 			
@@ -191,19 +191,19 @@ BOOST_AUTO_TEST_SUITE(UpwindGradientTestSuite)
 				for (int d = 0; d < grid_dim; d++)
 				{
 					// Analytical gradient
-					g_dist.getProp<df_gaussian>(key)[d] = hermite_polynomial(p.get(d), sigma, 1) * g_dist.getProp<f_gaussian>(key);
+					g_dist.getProp<dF_GAUSSIAN>(key)[d] = hermite_polynomial(p.get(d), sigma, 1) * g_dist.getProp<F_GAUSSIAN>(key);
 				}
 				++dom;
 			}
 			
 			// Upwind gradient with specific convergence order, not becoming one-sided at the boundary
-			get_upwind_gradient<f_gaussian, Sign, df_upwind>(g_dist, convergence_order, false);
+			get_upwind_gradient<F_GAUSSIAN, VELOCITY, dF_UPWIND>(g_dist, convergence_order, false);
 			
 			// Get the error between analytical and numerical solution
-			get_relative_error<df_upwind, df_gaussian, Error>(g_dist);
+			get_relative_error<dF_UPWIND, dF_GAUSSIAN, ERROR>(g_dist);
 			
 			LNorms<double> lNorms;
-			lNorms.get_l_norms_grid<Error>(g_dist);
+			lNorms.get_l_norms_grid<ERROR>(g_dist);
 
 			if (N==32) BOOST_CHECK_MESSAGE(lNorms.l2 <= 0.03215172234342 + EPSILON, "Checking L2-norm upwind gradient "
 																				"order "
@@ -229,7 +229,7 @@ BOOST_AUTO_TEST_SUITE(UpwindGradientTestSuite)
 			{
 				const size_t sz[grid_dim] = {N, N, N};
 				grid_in_type g_dist(sz, box, ghost);
-				g_dist.setPropNames({"f_gaussian", "Sign", "df_gaussian", "df_upwind", "Error"});
+				g_dist.setPropNames({"F_GAUSSIAN", "VELOCITY", "dF_GAUSSIAN", "dF_UPWIND", "ERROR"});
 				
 				auto gdom = g_dist.getDomainGhostIterator();
 				while (gdom.isNext())
@@ -237,9 +237,9 @@ BOOST_AUTO_TEST_SUITE(UpwindGradientTestSuite)
 					auto key = gdom.get();
 					Point<grid_dim, double> p = g_dist.getPos(key);
 					// Initialize grid and ghost with gaussian function
-					g_dist.getProp<f_gaussian>(key) = gaussian(p, mu, sigma);
+					g_dist.getProp<F_GAUSSIAN>(key) = gaussian(p, mu, sigma);
 					// Initialize the sign of the gaussian function
-					g_dist.getProp<Sign>(key) = sgn(g_dist.getProp<f_gaussian>(key));
+					g_dist.getProp<VELOCITY>(key) = sgn(g_dist.getProp<F_GAUSSIAN>(key));
 					++gdom;
 				}
 				
@@ -252,7 +252,7 @@ BOOST_AUTO_TEST_SUITE(UpwindGradientTestSuite)
 					for (int d = 0; d < grid_dim; d++)
 					{
 						// Analytical gradient
-						g_dist.getProp<df_gaussian>(key)[d] = hermite_polynomial(p.get(d), sigma, 1) * g_dist.getProp<f_gaussian>(key);
+						g_dist.getProp<dF_GAUSSIAN>(key)[d] = hermite_polynomial(p.get(d), sigma, 1) * g_dist.getProp<F_GAUSSIAN>(key);
 					}
 					++dom;
 				}
@@ -260,12 +260,12 @@ BOOST_AUTO_TEST_SUITE(UpwindGradientTestSuite)
 				for (int convergence_order = 1; convergence_order <=5; convergence_order +=2 )
 				{
 					// Upwind gradient with specific convergence order, not becoming one-sided at the boundary
-					get_upwind_gradient<f_gaussian, Sign, df_upwind>(g_dist, convergence_order, false);
+					get_upwind_gradient<F_GAUSSIAN, VELOCITY, dF_UPWIND>(g_dist, convergence_order, false);
 					// Get the error between analytical and numerical solution
-					get_relative_error<df_upwind, df_gaussian, Error>(g_dist);
+					get_relative_error<dF_UPWIND, dF_GAUSSIAN, ERROR>(g_dist);
 					
 					LNorms<double> lNorms;
-					lNorms.get_l_norms_grid<Error>(g_dist);
+					lNorms.get_l_norms_grid<ERROR>(g_dist);
 
 					if(N==32)
 					{
@@ -280,6 +280,96 @@ BOOST_AUTO_TEST_SUITE(UpwindGradientTestSuite)
 							case 5:
 								BOOST_CHECK_MESSAGE(lNorms.l2 <= 0.02285996528578 + EPSILON, "Checking L2-norm upwind gradient order " + std::to_string(convergence_order));
 
+								break;
+							default:
+								std::cout << "Checking only implemented for convergence order 1, 3 and 5." << std::endl;
+						}
+					}
+				}
+				
+			}
+		}
+	}
+	
+	BOOST_AUTO_TEST_CASE(Upwind_gradient_3D_vector_velocity_test)
+	{
+		{
+			const size_t grid_dim  = 3;
+			const double box_lower = -1.0;
+			const double box_upper = 1.0;
+			Box<grid_dim, double> box({box_lower, box_lower, box_lower}, {box_upper, box_upper, box_upper});
+			Ghost<grid_dim, long int> ghost(3);
+			typedef aggregate<double, double[grid_dim], Point<grid_dim, double>, Point<grid_dim, double>, double> props;
+			typedef grid_dist_id<grid_dim, double, props> grid_in_type;
+			
+			double mu = 0.5 * (box_upper - abs(box_lower));
+			double sigma = 0.3 * (box_upper - box_lower);
+			
+			size_t N = 32;
+//			for(size_t N = 32; N <=256; N *=2)
+			{
+				const size_t sz[grid_dim] = {N, N, N};
+				grid_in_type g_dist(sz, box, ghost);
+				g_dist.setPropNames({"F_GAUSSIAN", "Velocity", "dF_GAUSSIAN", "dF_UPWIND", "ERROR"});
+				
+				auto gdom = g_dist.getDomainGhostIterator();
+				while (gdom.isNext())
+				{
+					auto key = gdom.get();
+					Point<grid_dim, double> p = g_dist.getPos(key);
+					// Initialize grid and ghost with gaussian function
+					g_dist.getProp<F_GAUSSIAN>(key) = gaussian(p, mu, sigma);
+					++gdom;
+				}
+				
+				auto dom = g_dist.getDomainIterator();
+				while (dom.isNext())
+				{
+					auto key = dom.get();
+					Point<grid_dim, double> p = g_dist.getPos(key);
+					
+					for (int d = 0; d < grid_dim; d++)
+					{
+						// Analytical gradient
+						g_dist.getProp<dF_GAUSSIAN>(key)[d] = hermite_polynomial(p.get(d), sigma, 1) * g_dist.getProp<F_GAUSSIAN>(key);
+						// Initialize the velocity vector field
+						g_dist.getProp<VELOCITY>(key)[d] = g_dist.getProp<dF_GAUSSIAN>(key)[d];
+					}
+					++dom;
+				}
+				
+				for (int convergence_order = 1; convergence_order <=5; convergence_order +=2 )
+				{
+					// Upwind gradient with specific convergence order, not becoming one-sided at the boundary
+					get_upwind_gradient<F_GAUSSIAN, VELOCITY, dF_UPWIND>(g_dist, convergence_order, false);
+					// Get the error between analytical and numerical solution
+					get_relative_error<dF_UPWIND, dF_GAUSSIAN, ERROR>(g_dist);
+					
+					LNorms<double> lNorms;
+					lNorms.get_l_norms_grid<ERROR>(g_dist);
+					
+					if(N==32)
+					{
+						switch(convergence_order)
+						{
+							case 1:
+								BOOST_CHECK_MESSAGE(lNorms.l2 <= 0.38954184748194 + EPSILON, "Checking L2-norm upwind"
+																							 " gradient with vector "
+																							 "velocity order " +
+																							 std::to_string(convergence_order));
+								break;
+							case 3:
+								BOOST_CHECK_MESSAGE(lNorms.l2 <= 0.08667855716144 + EPSILON, "Checking L2-norm upwind"
+																							 " gradient with "
+																							 "vector velocity order " +
+																							 std::to_string(convergence_order));
+								break;
+							case 5:
+								BOOST_CHECK_MESSAGE(lNorms.l2 <= 0.02285996528578 + EPSILON, "Checking L2-norm upwind"
+																							 " gradient with "
+																							 "vector velocity order " +
+																							 std::to_string(convergence_order));
+								
 								break;
 							default:
 								std::cout << "Checking only implemented for convergence order 1, 3 and 5." << std::endl;
